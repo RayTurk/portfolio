136b7ef3f437931fdbedec71235781ef1aea6fa5
Raymond <46539130+RayTurk@users.noreply.github.com>
2024-11-16 12:32:21 -0500

    Updated config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  name: "portfolio",
+  homepage: "https://RayTurk.github.io/portfolio",
+  scripts: {
+    predeploy: "npm run build",
+    deploy: "gh-pages -d dist"
+  },
   base: '/portfolio/',
   plugins: [react()],
   resolve: {
@@ -13,5 +19,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  }
+  },
+
+
 })
